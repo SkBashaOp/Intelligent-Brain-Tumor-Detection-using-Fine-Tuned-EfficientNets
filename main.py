cb2c2eb1264ceb2a33ba6eb2355ae260ba22ea94
SkBashaOp <hussainfractions@gmail.com>
2026-03-27 21:07:28 +0530

    fix main navigation

--- main.py
+++ main.py
@@ -1,47 +1,20 @@
-# Importing the necessary Python modules.
 import streamlit as st
-
-
-st.markdown("""
-<h1 style='text-align: center; color: #4CAF50;'>
-🧠 NeuroScanAI
-</h1>
-<p style='text-align: center; font-size:18px;'>
-AI-powered Brain Tumor Detection using EfficientNetB3
-</p>
-<hr>
-""", unsafe_allow_html=True)
-
-
-# Configure the app
-st.set_page_config(
-    page_title = 'MRI-Data-Analysis',
-    page_icon = 'brain',
-    initial_sidebar_state = 'auto'
-)
 
 # Import pages
 from Tabs import home, BT
 
+# Page config
+st.set_page_config(page_title="MRI Brain Tumor Detection", layout="wide")
 
-
-# Dictionary for pages
+# Navigation
 Tabs = {
     "Home": home,
-    "Brain Tumor":BT
-   
-    
+    "Brain Tumor": BT
 }
 
-
-
-# Create a sidebar
-# Add title to sidear
+# Sidebar
 st.sidebar.title("Navigation")
+selection = st.sidebar.radio("Go to", list(Tabs.keys()))
 
-# Create radio option to select the page
-page = st.sidebar.radio("Pages", list(Tabs.keys()))
-
-
-# Call the app funciton of selected page to run
-Tabs[page].app()
+# Load selected page
+Tabs[selection].app()
